59f0aa641336213104ad37f967eff829c7a20fa9
Vikrant Chaudhary <nasa42@gmail.com>
2009-05-10 08:23:01 +0000

    just another commit.

--- lib/kopal_pref.rb
+++ lib/kopal_pref.rb
@@ -1,8 +1,8 @@
 class KopalPref
   def self.method_missing(method, *args)
-    if method =~ /-$/
+    if method =~ /=$/
       k = KopalPreference.find_or_initialize_by_preference_name(method.gsub('=', ''))
-      k.preference_text = args[0]
+      k.preference_text = args[0].to_s
       k.save!
     else
       k = KopalPreference.find_by_preference_name(method)
--- test/kopal_test.rb
+++ test/kopal_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class KopalTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def test_schema_has_loaded_successfully
+    assert_equal [], KopalFriend.all
+    assert_equal 1, KopalPreference.all.length
+    assert_equal 'last_migration_revision', KopalPreference.first.preference_name
   end
+
+  def test_KopalPref_works_good
+    KopalPref.testing_is = "going good"
+    assert_equal "going good", KopalPreference.find_by_preference_name('testing_is').preference_text
+    assert_equal "going good", KopalPref.testing_is
+    KopalPref.everything_is_string = 1
+    assert_equal "1", KopalPreference.find_by_preference_name('testing_is').preference_text
+  end
+
+  def test_a_migration_has_run
+    before = KopalPref.last_migration_revision
+    #run a dummy migration
+    assert_equal before+1, KopalPref.last_migration_revision #won't work, they are dated not numbered.
+  end
+  
 end
