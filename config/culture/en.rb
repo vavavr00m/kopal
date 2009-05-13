dd277cb679705a36243a9940a65fc2fed520288f
Vikrant Chaudhary <nasa42@gmail.com>
2009-05-13 09:47:51 +0000

    added organise controller, authorise method. added ISO country codes and UN/LOCODEs.

---
+++ config/culture/en.rb
@@ -0,0 +1,12 @@
+{
+  :en => {
+    :hello => "Hello, world!",
+    :name => "Name",
+    :aliases => "Aliases",
+    :about_me => "About me",
+    :gender => "Gender",
+    :date_of_birth => "Date of Birth",
+    :country => "Country"
+  }
+}
+
