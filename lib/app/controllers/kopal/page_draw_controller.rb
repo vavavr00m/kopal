96733d5bf2a41b443b225720051b9905792fcffa
Vikrant Chaudhary <nasa42@gmail.com>
2009-08-30 18:13:21 +0530

    Implemented Profile Pages. Heavily imported and heavily edited. Most of the code comes from my previous project Colourpark. Highly edited for logic and coding style. But some may remain. For example, most of the symbols are in :'name' format, since before I used strings but now I'm preferring symbols.

---
+++ lib/app/controllers/kopal/page_draw_controller.rb
@@ -0,0 +1,295 @@
+class Kopal::PageDrawController < Kopal::ApplicationController
+  helper Kopal::Helper::PageDrawHelper
+
+# params[:page_id] has preference over params[:page].
+#
+#LATER: Remove inline JavaScript.
+#LATER: Make current profile pages as "Social Profile", "Interests" etc. as page
+#widgets under section "Add widget -> Profile ". Users can also create their own
+#page widgets which display a profile of the user on a particular subject.
+#
+#LATER: Add element - "Table".
+
+
+	before_filter :authorise, :page_draw_init
+  before_filter :initialise_atpage, :except => ['index', 'create_page']
+
+  def index
+    redirect_to Kopal.route.page
+  end
+
+  def create_page
+    title = "new-page"
+    p = Kopal::ProfilePage.new
+    p.page_name = title
+    i = 1
+    until p.save
+      p.page_name = title + "-#{i += 1}"
+      raise if i > 10000 #Too much looping, save from infinity.
+    end
+    redirect_to Kopal.route.page_edit :page => p.page_name
+  end
+
+  def rename_page
+    if request.post?
+      old_name = @page.page_name
+      @page.page_name = params[:page_name]
+      if @page.save
+        flash[:highlight] = "Renamed page."
+        redirect_to Kopal.route.page_draw :action => 'edit', :page => @page.to_s
+        return
+      end
+      @page.page_name = old_name
+    end
+  end
+
+	#LATER: In lists a option - View this list as numbered, with bullets/squares, nothing (default)
+	def edit
+		redirect_to :action => 'index' and return if params[:page].blank?
+    @_page.title = [params[:page], "Edit Page"]
+		@already_present_elements = ''
+    @page.elements.each { |e|
+      e[:'heading'] = 'Click to edit heading' if e[:'heading'].blank?
+      @already_present_elements << render_element_to_string(e)
+    }
+	end
+
+	#LATER: all these methods to one method insert_element
+	def insert_element_text
+		element = {}
+		element[:'type'] = 'text'
+		element[:'heading'] = 'Heading for note'
+		element[:'text'] = 'New text note'
+		create_and_render_element element
+	end
+
+	def insert_element_list
+		element = {}
+		element[:'type'] = 'list'
+		element[:'heading'] = 'My List —'
+		element[:'entry'] = ['New list element', 'New list element']
+		create_and_render_element element
+	end
+
+	def insert_element_pair
+		element = {}
+		element[:'type'] = 'pair'
+		element[:'heading'] = 'My Profile —'
+		element[:'entry'] = [ {:'heading' => 'This is heading', :'text' => 'This is the text'},
+			{:'heading' => 'This is another heading', :'text' => "This is another text \n in multiple lines."}
+			]
+		create_and_render_element element
+	end
+
+	def insert_element_todo
+		element = {}
+		element[:'type'] = 'todo'
+		element[:'heading'] = 'My to-dos'
+		element[:'entry'] = [ {:'text' => 'My to-do element', :'status' => 'active'},
+      {:'text' => 'another to-do element', :'status' => 'done'} ]
+		create_and_render_element element
+	end
+
+	def insert_element_separator
+		element = {}
+		element[:'type'] = 'separator'
+		element[:'heading'] = 'Separator'
+		create_and_render_element element
+	end
+
+	#LATER: All these methods to one method - insert_element_child
+	def insert_list_element
+    @element = @page.page_text[:element][params[:element_id].to_i]
+		return unless @element and @element[:'type'] == 'list'
+		@element[:entry] << 'New list element'
+		@page.save!
+    render :action => 'insert_child_element'
+	end
+
+	def insert_pair_element
+		@element = @page.page_text[:element][params[:element_id].to_i]
+		return unless @element and @element[:type] == 'pair'
+    @element[:entry] << { :'heading' => :'Element heading',
+        :'text' => 'Element text' }
+		@page.save!
+		render :action => 'insert_child_element'
+	end
+
+	def insert_todo_element
+		@element = @page.page_text[:element][params[:element_id].to_i]
+		return unless @element and @element[:'type'] == 'todo'
+		@element[:entry] << { :'text' => 'New to-do', :'status' => 'active' }
+		@page.save!
+    render :action => 'insert_child_element'
+	end
+
+	def save_element_heading #Common for all elements
+		#This is assumed that all elements have a field - heading
+		@page.page_text[:element][params[:id].to_i][:'heading'] = params[:value]
+		@page.save!
+		@heading = params[:value].blank?() ? '[Click to edit heading]' : params[:value]
+		render :inline => "<%= h @heading %>"
+	end
+
+	def save_element_child
+		return if params[:type].blank?
+		@element = @page.page_text[:element][params[:element_id].to_i]
+		@child_index = params[:child_index].to_i
+		return unless @element[:'type'] == params[:type] #sign_error()
+		@to_render = case params[:type]
+			when 'pair':
+				if(params[:sub_type] == 'text')
+					@element[:'entry'][@child_index][:'text'] = params[:value]
+				elsif(params[:sub_type] == 'heading')
+					@element[:'entry'][@child_index][:'heading'] = params[:value]
+				end
+		end
+		@page.save!
+    @to_render.blank?() ? @to_render = "[Click to edit]" : nil
+		render :inline => "<%= params[:sub_type] == 'text' ?
+      wikify(@to_render) : h(@to_render) %>"
+	end
+
+	#LATER: !DRY at present, integrate all these methods to above method save_element_child
+	def save_text_text #Make use of Highlighting
+    element = @page.page_text[:element][params[:id].to_i]
+		unless element or element[:'type'] == 'text'
+			return false
+		end
+		element[:'text'] = params[:value]
+		@page.save!
+    @element = element
+		render :inline => "<%= wikify @element[:'text'] %>"
+	end
+
+	def save_list_element
+    @element = @page.page_text[:element][params[:id].to_i]
+    @child_index = params[:child_index].to_i
+		return false unless @element and @element[:'type'] == 'list' and
+      @element[:entry][@child_index]
+		@element[:entry][@child_index] = params[:value]
+		@page.save!
+		render :inline => "<%= wikify @element[:entry][@child_index] %>"
+	end
+
+  #Why passing parameters as p1_p2 format? Because back then when I wrote this
+  #code (1 year or so), there was a problem with something that more than 1 parameters
+  #were passed as p1=v1&amp;p2=v2. so the second paramater was actually params[:"amp;p2"],
+  #(or may be because I didn't understand rails much then).
+  #This situation doesn't seem to occur now.
+	def save_todo_element
+		todo_id, @child_index = params[:id].split('_').map { |e| e.to_i}
+		@element = @page.page_text[:element][todo_id]
+		return unless @element and @element[:'type'] == 'todo' and @element[:entry][@child_index]
+		@element[:entry][@child_index][:text] = params[:value]
+		@page.save!
+		render :inline => "<%= wikify @element[:entry][@child_index][:text] %>"
+	end
+
+	def save_todo_element_status
+		todo_id, @child_index, @child_status = params[:id].split('_')
+    @child_index = @child_index.to_i
+		@element = @page.page_text[:element][todo_id.to_i]
+    #OPTIMIZE: sign_error() instead of just returning.
+		return unless @element and @element[:'type'] == 'todo' and @element[:entry][@child_index]
+		@element[:entry][@child_index][:'status'] = @child_status
+		@page.save!
+	end
+
+	def edit_table
+	end
+
+	def delete_element
+		@page.delete_element params[:id].to_i
+		render :nothing => true
+	end
+
+	#Deletes elements of lists, pair and todos
+	def delete_element_child
+    @element = @page.page_text[:element][params[:element_id].to_i]
+    @child_index = params[:child_index].to_i
+		unless @element and @element[:entry][params[:child_index].to_i]
+			render_status(403) and return
+		end
+		@element[:entry].delete_at(@child_index)
+		@page.save!
+	end
+
+	def delete_page
+		if request.post?
+			if request.xhr?
+				unless @page
+          flash[:notice] = "Can not find page - #{@page}"
+        else
+          @page.destroy
+          flash[:highlight] = "Page \"#{@page}\" has been deleted."
+        end
+			else
+				# code for deletion without xhr
+			end
+		end
+		redirect_2 Kopal.route.page
+	end
+
+	def update_page_description_aj
+		return false if params[:value].empty? or !@page
+		@page.page_description = params[:value]
+    @page.save!
+		render :text => @page.page_description
+	end
+
+	def sort_element
+		unless params[:PageEdit] or params[:PageEdit].instance_of? Array
+			return
+		end
+		@page.page_text[:meta][:sorting_order] = params[:PageEdit].map { |x| x.to_i}
+		@page.save
+		render :nothing => true
+	end
+
+private
+	def page_draw_init # May also use initialize() here but has a doubt that will still the action won't execute if it returns false?
+    @page = Kopal::ProfilePage.find_by_page_name params[:page] if params[:page]
+		@_page.title <<= @page.page_name if @page
+    @_page.include_prototype
+    @_page.include_scriptaculous
+	end
+
+  #initialise @page
+  def initialise_atpage
+		if params[:page_id]
+			page_by_id_exists?
+		elsif params[:page]
+			page_exists?
+		end
+    unless @page
+      flash[:notice] = "Can not find page - #{@page}"
+      redirect_2 Kopal.route.page_draw
+      return false
+    end
+  end
+
+	def page_by_id_exists?
+		return if params[:page_id].blank?
+		@page = Kopal::ProfilePage.find_by_id(params[:page_id])
+	end
+
+	def page_exists?
+		return if params[:page].blank?
+		@page = Kopal::ProfilePage.find_by_page_name(params[:page])
+	end
+
+	def render_element_to_string element
+		@element = element #to be used by element_layout.rhtml
+		return render_to_string(:action => 'element_layout', :layout => false)
+	end
+
+	def create_and_render_element element
+		@page.insert_element element
+		r = render_element_to_string(element)
+		render :update do |page|
+			page.insert_html :top, 'PageEdit', r
+		end
+	end
+
+end
--- lib/kopal/helper/page_helper.rb
+++ lib/kopal/helper/page_helper.rb
@@ -11,11 +11,65 @@ module Kopal::Helper::PageHelper
     end
   end
 
+  def wikify string, markup = "html"
+    string = "#{string}"
+    #Convert
+    # use following when it is supported by IE and Firefox
+    #"<span style=\"white-space:pre-line;\">#{string}</span>"
+    # from simple_format() in ActionView::Helpers::TextHelper
+    string.gsub!(/\r\n?/, "\n")                     # \r\n and \r -> \n
+    string.gsub!(/\n\n+/, "</p>\n\n<p>")            # 2+ newline  -> paragraph
+    string.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />')  # 1 newline   -> br
+		#TODO: change http://, www. and all to <a href....
+		string = sanitize(string)
+	end
+
+  #pass an array of hashes as - {:label, :link, :weight, :title}, weight can be any digit.
+  #options - {:max_font_size, :limit}
+  def as_cloud ar, options = {} #ar => array
+    max_font_size = options[:max_font_size] || 36
+    min_font_size = 12
+    limit = options[:limit]
+    #named weight not mass, since its relative.
+    biggest = 0
+    ar.each { |a|
+      biggest = a[:weight] if a[:weight] > biggest
+    }
+    if limit
+      sorted_array = ar.sort { |x,y| y[:weight] <=> x[:weight]}
+      sorted_array = sorted_array.slice(0...(limit.to_i))
+      ar = ar & sorted_array #"sorted_array & a" will get them sorted.
+    end
+    r = ''
+    ar.each { |a|
+      #should decrease exponentially
+      m = Math.sqrt(a[:weight])*max_font_size/Math.sqrt(biggest)
+      m = m.to_int
+      m = min_font_size if m < min_font_size
+      m = max_font_size if m > max_font_size
+
+      a[:'title'] = a[:label] if a[:'title'].blank?
+      a[:label] = h(a[:label])
+
+      r << link_to(a[:label], a[:link], :title => a[:title],
+        :class => "cloud", :style => "font-size:#{m}px;")
+    }
+    return r
+	end
+
   def gravatar_url email
     require 'md5'
     hash = MD5.md5 email
     return "http://www.gravatar.com/avatar/#{hash}.jpeg?s=120"
   end
+
+  def whitespaced s, from = "_"
+		return s.gsub(from, ' ')
+	end
+
+	def underscored s, from = " "
+		return s.gsub(from, '_')
+	end
 
   def format_date date, without_html = false
     return date unless date.is_a? Date or date.is_a? Time or date.is_a? DateTime
