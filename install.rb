87b90ef46e35e210ca6680daa5c98270aa6f7588
Vikrant Chaudhary <nasa42@gmail.com>
2009-09-12 13:15:33 +0530

    Minor syntax improvement.

--- install.rb
+++ install.rb
@@ -4,4 +4,4 @@ current_dir = File.dirname(__FILE__)
 File.rename(current_dir, "#{current_dir}/../kopal") if
   File.basename(current_dir) == 'hg'
 
-puts "\n\nNote: **** Please run \"rake kopal:update RAILS_ENV=production\" to update Kopal. ****\n"
+puts "\nNote: **** Please run \"rake kopal:update RAILS_ENV=production\" to update Kopal. ****\n\n"
