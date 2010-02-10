c3cc509b1bf8a701c5728ae1517bb7a1c6dc8e54
Vikrant Chaudhary <nasa42@gmail.com>
2010-02-10 16:29:04 +0530

    No more dependency on SQLite3 since switched to app's database.

--- config_dependency.rb
+++ config_dependency.rb
@@ -1,5 +1,4 @@
 Rails.configuration.gem 'ruby-openid', :lib => 'openid', :version => '>= 2.1.7'
-Rails.configuration.gem 'sqlite3-ruby', :lib => 'sqlite3'
 #Remove following dependencies and make code utilise them only if they are available.
 Rails.configuration.gem 'gemcutter' #For will_paginate.
 Rails.configuration.gem 'will_paginate'
