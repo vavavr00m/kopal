b4a504215989e0821aec906cbee709bce1a17143
Vikrant Chaudhary <nasa42@gmail.com>
2010-02-17 10:51:52 +0530

    Tried to write some network test but can't get it to work.

---
+++ test/network/kopal_connect_test.rb
@@ -0,0 +1,4 @@
+require File.dirname(__FILE__) + '/../network_test_helper'
+
+class Kopal::KopalConnectTest < Kopal::NetworkTestHelper
+end
---
+++ test/network/openid_test.rb
@@ -0,0 +1,21 @@
+require File.dirname(__FILE__) + '/../network_test_helper'
+
+class Kopal::OpenidTest < Kopal::NetworkTestHelper
+
+  def test_authentication_with_openid
+    #@request.host = 'a.kopal.test'
+    identifier = "http://b.kopal.test:#{server_port}/profile/"
+    puts "Openid to #{identifier}"
+    #Can't get to work.
+    #Maybe we need to send the request through server, and listen through it.
+    #since b.kopal.test might make a request back to it.
+    get "/profile/home/openid/", {:openid_identifier => identifier}
+    assert_response :redirect, response.body
+
+    follow_redirect!
+  end
+
+  def test_openid_server
+    
+  end
+end
