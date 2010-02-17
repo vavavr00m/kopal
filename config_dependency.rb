55e2d2d4bc7935b62942c1e402fcc2eb5d2dcfba
Vikrant Chaudhary <nasa42@gmail.com>
2010-02-17 10:51:10 +0530

    Fixing LoadError - no such file to load -- gemcutter

--- config_dependency.rb
+++ config_dependency.rb
@@ -1,4 +1,4 @@
 Rails.configuration.gem 'ruby-openid', :lib => 'openid', :version => '>= 2.1.7'
 #Remove following dependencies and make code utilise them only if they are available.
-Rails.configuration.gem 'gemcutter' #For will_paginate.
+Rails.configuration.gem 'gemcutter', :lib => false #For will_paginate.
 Rails.configuration.gem 'will_paginate'
