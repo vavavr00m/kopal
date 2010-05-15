c78db17f53741f61085319d6e4b3e0b1d4e6511a
Vikrant Chaudhary <nasa42@gmail.com>
2010-05-15 14:13:19 +0530

    Added 3 models ProfilePage, PageWidget and ProfileStore.

---
+++ lib/app/models/kopal/profile_store.rb
@@ -0,0 +1,20 @@
+class Kopal::ProfileStore < Kopal::Model
+  set_table_name "#{name_prefix}profile_store"
+
+  belongs_to :widget, :class => 'Kopal::PageWidget',
+    :primary_key => 'widget_key', :foreign_key => 'widget_key'
+
+  before_validation_on_create :assign_scope
+
+  validates_presence_of :widget_key
+  #TODO: Rename "record" to something more meaningful.
+  validates_presence_of :record_name
+  validates_presence_of :scope
+  validates_uniqueness_of :record_name, :scope => 'widget_key'
+
+private
+
+  def assign_scope
+    self[:scope] = scope.to_i
+  end
+end
