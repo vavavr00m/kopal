b7d713ae059669cf9b1af3c108a9af816369d37e
Vikrant Chaudhary <nasa42@gmail.com>
2010-07-01 20:26:11 +0530

    [small] some text change in comment.

--- lib/core_extension/string.rb
+++ lib/core_extension/string.rb
@@ -1,7 +1,7 @@
 class String
 
   #If +to_s().blank?()+ then provide a default text.
-  #Good, if "self" is a_very_long_variable_name, which Kopal has many.
+  #Good, if "self" is a_very_long_variable_name_or_method_call, which Kopal has many.
   def with_default default_string
     blank?() ? default_string  : self
   end
