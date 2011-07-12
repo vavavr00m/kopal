a007526ff40722b0463af81ae8080e008d5c3a4c
Vikrant Chaudhary <nasa42@gmail.com>
2011-07-12 20:59:57 +0530

    Added example_app using command "rails new example_app --skip-git --skip-active-record". So far, the development Rails application lived outside of the repository, but now getting it inside the repository as "rails plugin new engine --mountable" creates a nested application at test/dummy anyway.

---
+++ example_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExampleApp::Application.config.secret_token = 'f64937380936d350a1e2443d67979794a2e9d01e0760592bb2f208d5445769e54dcdd789b61dc7bd649c40571916b104d37b76a2aa91d9996203ee5f62f20c35'
